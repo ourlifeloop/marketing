133c39e01436fd0a8d3acfd0e8d90741e80ce021
Mitchel Pigsley <mpigsley@gmail.com>
2019-06-06 15:45:00 -0500

    Authenticate with mailchimp to subscribe email

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -3,16 +3,22 @@ const rp = require('request-promise');
 exports.handler = (event, context, callback) => {
   const { email } = event.queryStringParameters;
   if (!email) {
-    return callback(null, { statusCode: 400 });
+    return callback(null, { body: 'Invalid Email', statusCode: 400 });
   }
 
   return rp(process.env.MAILCHIMP_API, {
     method: 'POST',
     body: JSON.stringify({ email_address: email, status: 'subscribed' }),
+    auth: {
+      user: process.env.MAILCHIMP_USER,
+      pass: process.env.MAILCHIMP_PASS,
+    },
   })
-    .then(() => callback(null, { statusCode: 200 }))
-    .catch(err => {
-      console.log(err);
-      callback(null, { statusCode: 500 });
-    });
+    .then(() => callback(null, { body: 'Success', statusCode: 200 }))
+    .catch(err =>
+      callback(null, {
+        body: JSON.parse(err.response.body).title,
+        statusCode: 500,
+      }),
+    );
 };
