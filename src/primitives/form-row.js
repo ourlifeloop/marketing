9df96ad4b7827c7815f5ec4fe9dc87e31a437446
Mitchel Pigsley <mpigsley@gmail.com>
2019-06-14 09:49:23 -0500

    Collapse form rows on small devices

--- src/primitives/form-row.js
+++ src/primitives/form-row.js
@@ -3,15 +3,19 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import FlexContainer from './flex-container';
+import { useDevice } from '../utils/effects';
 
 import styles from './form-row.module.scss';
 
 export default function FormRow({ children }) {
+  const { isMini } = useDevice();
   return (
-    <FlexContainer>
+    <FlexContainer direction={isMini ? 'column' : 'row'}>
       {React.Children.map(children, child =>
         React.cloneElement(child, {
-          className: classNames(child.className, styles.child),
+          className: classNames(child.className, styles.child, {
+            [styles.childCollapsed]: isMini,
+          }),
         }),
       )}
     </FlexContainer>
