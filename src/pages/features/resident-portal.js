2e35d376fc504580312358afe2d6ea1d3cb528ff
Mitchel Pigsley <mpigsley@gmail.com>
2019-06-14 13:39:39 -0500

    Resident portal feature sections

--- src/pages/features/resident-portal.js
+++ src/pages/features/resident-portal.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import FeatureWrapper from '../../components/feature-wrapper';
 import TitleSection from '../../primitives/title.section';
+import ImageSection from '../../primitives/image-section';
 
-export default ({ location }) => (
+export default ({ data, location }) => (
   <FeatureWrapper pathname={location.pathname}>
     <TitleSection header="Resident Portal">
       <p>
@@ -14,5 +15,42 @@ export default ({ location }) => (
         vel, pulvinar condimentum tellus.
       </p>
     </TitleSection>
+    <ImageSection
+      rightImage
+      title="Maximize Their Independence"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus vitae augue ornare ultrices. Nullam a elementum nisi. Aenean pulvinar finibus pharetra. Integer ornare euismod enim id dictum. Donec id nisi id sapien sagittis pharetra. Quisque mi purus, fermentum mattis tortor vel, pulvinar condimentum tellus."
+      image={data.independence.childImageSharp.fluid}
+    />
+    <ImageSection
+      leftImage
+      title="Ease Of Use"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus vitae augue ornare ultrices. Nullam a elementum nisi. Aenean pulvinar finibus pharetra. Integer ornare euismod enim id dictum. Donec id nisi id sapien sagittis pharetra. Quisque mi purus, fermentum mattis tortor vel, pulvinar condimentum tellus."
+      image={data.independence.childImageSharp.fluid}
+    />
+    <ImageSection
+      rightImage
+      title="Keep Residents In The Loop"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus vitae augue ornare ultrices. Nullam a elementum nisi. Aenean pulvinar finibus pharetra. Integer ornare euismod enim id dictum. Donec id nisi id sapien sagittis pharetra. Quisque mi purus, fermentum mattis tortor vel, pulvinar condimentum tellus."
+      image={data.independence.childImageSharp.fluid}
+    />
+    <ImageSection
+      leftImage
+      title="Built In Reminders"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus vitae augue ornare ultrices. Nullam a elementum nisi. Aenean pulvinar finibus pharetra. Integer ornare euismod enim id dictum. Donec id nisi id sapien sagittis pharetra. Quisque mi purus, fermentum mattis tortor vel, pulvinar condimentum tellus."
+      image={data.independence.childImageSharp.fluid}
+    />
   </FeatureWrapper>
 );
+
+export const query = graphql`
+  query {
+    independence: file(relativePath: { eq: "maximize-independence.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 860) {
+          ...GatsbyImageSharpFluid
+          presentationWidth
+        }
+      }
+    }
+  }
+`;
