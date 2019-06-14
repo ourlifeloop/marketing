b1f28a3257eaedae6f4d089d0773de0f85623f27
Mitchel Pigsley <mpigsley@gmail.com>
2019-06-14 13:28:03 -0500

    Messaging feature sections

--- src/pages/features/messaging.js
+++ src/pages/features/messaging.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import FeatureWrapper from '../../components/feature-wrapper';
 import TitleSection from '../../primitives/title.section';
+import ImageSection from '../../primitives/image-section';
 
-export default ({ location }) => (
+export default ({ data, location }) => (
   <FeatureWrapper pathname={location.pathname}>
-    <TitleSection header="Communication & Messages">
+    <TitleSection header="Communication">
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus
         vitae augue ornare ultrices. Nullam a elementum nisi. Aenean pulvinar
@@ -14,5 +15,58 @@ export default ({ location }) => (
         vel, pulvinar condimentum tellus.
       </p>
     </TitleSection>
+    <ImageSection
+      rightImage
+      title="Seamless Flow of Communication"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus vitae augue ornare ultrices. Nullam a elementum nisi. Aenean pulvinar finibus pharetra. Integer ornare euismod enim id dictum. Donec id nisi id sapien sagittis pharetra. Quisque mi purus, fermentum mattis tortor vel, pulvinar condimentum tellus."
+      image={data.flow.childImageSharp.fluid}
+    />
+    <ImageSection
+      leftImage
+      title="Inform Your Staff"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus vitae augue ornare ultrices. Nullam a elementum nisi. Aenean pulvinar finibus pharetra. Integer ornare euismod enim id dictum. Donec id nisi id sapien sagittis pharetra. Quisque mi purus, fermentum mattis tortor vel, pulvinar condimentum tellus."
+      image={data.inform.childImageSharp.fluid}
+    />
+    <ImageSection
+      rightImage
+      title="Transparancy With Your Residents' Families"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus vitae augue ornare ultrices. Nullam a elementum nisi. Aenean pulvinar finibus pharetra. Integer ornare euismod enim id dictum. Donec id nisi id sapien sagittis pharetra. Quisque mi purus, fermentum mattis tortor vel, pulvinar condimentum tellus."
+      image={data.transparency.childImageSharp.fluid}
+    />
+    <ImageSection
+      leftImage
+      title="Residents Maintain Sense Of Independence"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam faucibus vitae augue ornare ultrices. Nullam a elementum nisi. Aenean pulvinar finibus pharetra. Integer ornare euismod enim id dictum. Donec id nisi id sapien sagittis pharetra. Quisque mi purus, fermentum mattis tortor vel, pulvinar condimentum tellus."
+      image={data.transparency.childImageSharp.fluid}
+    />
   </FeatureWrapper>
 );
+
+export const query = graphql`
+  query {
+    inform: file(relativePath: { eq: "inform-your-staff.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 860) {
+          ...GatsbyImageSharpFluid
+          presentationWidth
+        }
+      }
+    }
+    flow: file(relativePath: { eq: "seamless-flow.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 860) {
+          ...GatsbyImageSharpFluid
+          presentationWidth
+        }
+      }
+    }
+    transparency: file(relativePath: { eq: "transparency-with-family.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 860) {
+          ...GatsbyImageSharpFluid
+          presentationWidth
+        }
+      }
+    }
+  }
+`;
