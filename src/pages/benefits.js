eb2fe1f5cf3909a59e8457bb17defc70b25c5f08
Mitchel Pigsley <mpigsley@gmail.com>
2019-06-20 15:37:53 -0500

    Add hero image to benefits page

--- src/pages/benefits.js
+++ src/pages/benefits.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
 import SiteWrapper from '../components/site-wrapper';
+import HeroImage from '../primitives/hero-image';
 import Layout from '../primitives/layout';
 
-export default () => (
+export default ({ data }) => (
   <SiteWrapper title="Keeping family, residents and staff conneceted and engaged | LifeLoop">
+    <HeroImage
+      direction="right"
+      height="400px"
+      objectPosition="30% 85%"
+      image={data.hero.childImageSharp.fluid}
+      title="Set your community apart"
+      description="LifeLoop is a proven sales & marketing tool for your community. Our platform will set your community apart from the rest by empowering staff, engaging families and impacting residents."
+    />
     <Layout>
       <h1>Benefits</h1>
     </Layout>
   </SiteWrapper>
 );
+
+export const query = graphql`
+  query {
+    hero: file(relativePath: { eq: "features-hero.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 2000) {
+          ...GatsbyImageSharpFluid
+          presentationWidth
+        }
+      }
+    }
+  }
+`;
