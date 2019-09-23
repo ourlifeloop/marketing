d70ca17b281aff2a7c31a1c90998bfd46410bfba
Mitchel Pigsley <mpigsley@gmail.com>
2019-09-23 10:54:35 -0500

    Render all the different training modules on the main page

--- src/pages/training.js
+++ src/pages/training.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import Img from 'gatsby-image';
+import { Link, graphql } from 'gatsby';
 
 import TrainingWrapper from '../components/training-wrapper';
 import FlexContainer from '../primitives/flex-container';
 import TitleSection from '../primitives/title.section';
+import Section from '../primitives/section';
 import Button from '../primitives/button';
 
-// import styles from './training.module.scss';
+import styles from './training.module.scss';
 
 export default ({ data }) => {
   return (
@@ -29,7 +31,94 @@ export default ({ data }) => {
         </FlexContainer>
       }
     >
-      <h1>Training</h1>
+      <Section width="medium">
+        <h2 className={styles.topicHeader}>Browse by Topic</h2>
+        <FlexContainer flex="1" wrap>
+          <Link to="/" className={styles.topic}>
+            <Img fluid={data.gettingStarted.childImageSharp.fluid} />
+            <span>Getting Started</span>
+          </Link>
+          <Link to="/" className={styles.topic}>
+            <Img fluid={data.calendar.childImageSharp.fluid} />
+            <span>Activity Calendar</span>
+          </Link>
+          <Link to="/" className={styles.topic}>
+            <Img fluid={data.reports.childImageSharp.fluid} />
+            <span>Resident Management</span>
+          </Link>
+          <Link to="/" className={styles.topic}>
+            <Img fluid={data.photos.childImageSharp.fluid} />
+            <span>Photos</span>
+          </Link>
+          <Link to="/" className={styles.topic}>
+            <Img fluid={data.messages.childImageSharp.fluid} />
+            <span>Messaging</span>
+          </Link>
+          <Link to="/" className={styles.topic}>
+            <Img fluid={data.maintenance.childImageSharp.fluid} />
+            <span>Maintenance</span>
+          </Link>
+          <Link to="/" className={styles.topic}>
+            <Img fluid={data.transportation.childImageSharp.fluid} />
+            <span>Transportation</span>
+          </Link>
+          <Link to="/" className={styles.topic}>
+            <Img fluid={data.displays.childImageSharp.fluid} />
+            <span>Displays</span>
+          </Link>
+          <Link to="/" className={styles.topic}>
+            <Img fluid={data.residents.childImageSharp.fluid} />
+            <span>Resident Portal</span>
+          </Link>
+          <Link to="/" className={styles.topic}>
+            <Img fluid={data.family.childImageSharp.fluid} />
+            <span>Family Portal</span>
+          </Link>
+        </FlexContainer>
+      </Section>
     </TrainingWrapper>
   );
 };
+
+export const query = graphql`
+  fragment featureImage on File {
+    childImageSharp {
+      fluid(maxWidth: 280) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+
+  query {
+    calendar: file(relativePath: { eq: "features/calendar.png" }) {
+      ...featureImage
+    }
+    displays: file(relativePath: { eq: "features/displays.png" }) {
+      ...featureImage
+    }
+    family: file(relativePath: { eq: "features/family.png" }) {
+      ...featureImage
+    }
+    gettingStarted: file(relativePath: { eq: "features/getting-started.png" }) {
+      ...featureImage
+    }
+    maintenance: file(relativePath: { eq: "features/maintenance.png" }) {
+      ...featureImage
+    }
+    messages: file(relativePath: { eq: "features/messages.png" }) {
+      ...featureImage
+    }
+    photos: file(relativePath: { eq: "features/photos.png" }) {
+      ...featureImage
+    }
+    reports: file(relativePath: { eq: "features/reports.png" }) {
+      ...featureImage
+    }
+    residents: file(relativePath: { eq: "features/residents.png" }) {
+      ...featureImage
+    }
+    transportation: file(relativePath: { eq: "features/transportation.png" }) {
+      ...featureImage
+    }
+  }
+`;
