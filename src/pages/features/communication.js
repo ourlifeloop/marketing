0c78c0bab7c20cb8894f06755e94a74e493ffd7f
Mitchel Pigsley <mpigsley@gmail.com>
2020-06-15 16:17:52 -0500

    Update images/text on communication page

--- src/pages/features/communication.js
+++ src/pages/features/communication.js
@@ -10,39 +10,47 @@ export default ({ data, location }) => (
     pathname={location.pathname}
     title="Messaging software that keeps family, residents and staff connected at assisted living communities | LifeLoop"
   >
-    <TitleSection header="Communication">
+    <TitleSection header="Communication & Community Engagement">
       <p>
-        Communication between staff members, family members and residents has
-        never been easier or more efficient. The LifeLoop messaging platform
-        means you can communicate 24/7 from any device.
+        LifeLoop was founded in 2015 based on the immense need for more
+        communication between communities, residents and family members and that
+        continues to be our guiding light today. Because of this, our
+        communication platform is truly your all-in-one solution to create even
+        greater operational efficiencies.
       </p>
     </TitleSection>
     <ImageSection
       rightImage
-      title="Seamless Flow of Communication"
-      description="With the announcements feature in LifeLoop, you can broadcast messages to all staff, family and residents who are connected. Having an event at your community and want to inform family members? Send a broadcast announcement through LifeLoop and families will get notified in the family portal/mobile app as well as their email. Effortlessly add attachments, photos, or whatever you like."
+      title="Customize The Way You Communicate"
+      description="Send out branded messages and announcements to staff, residents, family, community vendors and more. Having an event at your community or implementing new procedures and want to inform family members? Send a broadcast announcement through LifeLoop and families will get notified in the family portal/mobile app as well as their email. Effortlessly add attachments, photos, or whatever you like."
       image={data.flow.childImageSharp.fluid}
     />
     <ImageSection
       leftImage
+      title="Distribute Secure Emergency Broadcast Messages"
+      description="Quickly keep essential audiences informed with critical updates and relevant information. Stay complaint with new guidelines and publish time-sensitive messages easily via text and email leaving staff free to care for residents. "
+      image={data.emergency.childImageSharp.fluid}
+    />
+    <ImageSection
+      rightImage
       title="Inform Your Staff"
       description="Staff can easily message back and forth within LifeLoop and have records of all contacts with other staff members, family members and residents. Allowing staff members to be in constant contact and in-the-know creates a strong community environment and helps with retention."
       image={data.inform.childImageSharp.fluid}
     />
     <ImageSection
-      rightImage
+      leftImage
       title="Transparancy With Your Residents' Families"
       description="The messaging feature allows family members the transparency they want and the access they deserve. With LifeLoop, all connected family members can see the staff directory and easily send messages to the appropriate staff member. Even better, family members can send messages to their loved one as well."
       image={data.transparency.childImageSharp.fluid}
     />
     <ImageSection
-      leftImage
+      rightImage
       title="Residents Maintain Sense Of Independence"
       description="Within the senior living industry, it is widely acknowledged that engaging the resident and their family members with technology can have a positive impact on the life of the resident, help alleviate social isolation and give your community a competitive edge. This platform was made custom with residents in mind and is easy for them to learn and use."
       image={data.independence.childImageSharp.fluid}
     />
     <ImageSection
-      rightImage
+      leftImage
       title="Customized Notifications"
       description="LifeLoop allows you to customize notifications and permissions based on each staff members individual preferences and needs. Notifications can be received as in-app notifications, texts and/or emails. Additionally, residents who are registered for an activity can receive a text message reminder 30 minutes before the activity takes place."
       image={data.notifications.childImageSharp.fluid}
@@ -63,6 +71,14 @@ export const query = graphql`
     flow: file(relativePath: { eq: "seamless-flow.png" }) {
       childImageSharp {
         fluid(maxHeight: 600) {
+          ...GatsbyImageSharpFluid
+          presentationWidth
+        }
+      }
+    }
+    emergency: file(relativePath: { eq: "emergency-broadcast.png" }) {
+      childImageSharp {
+        fluid(maxHeight: 599) {
           ...GatsbyImageSharpFluid
           presentationWidth
         }
