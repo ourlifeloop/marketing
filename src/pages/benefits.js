bdfd3b241864b77e729f6232a8abbbc216dec0be
Mitchel Pigsley <mpigsley@gmail.com>
2020-06-16 15:06:14 -0500

    Update Thomas Cloutier logo/position

--- src/pages/benefits.js
+++ src/pages/benefits.js
@@ -200,11 +200,11 @@ export default ({ data }) => {
       <Testimonials
         testimonials={[
           {
-            logo: 'cascadia',
+            logo: 'avamere',
             quote:
               "LifeLoop's valuable to us because the present and the future of our industry is to keep family members more and more involved in our residents' lives. Family members are naturally curious about what's going on with their loved ones, and LifeLoop is both a proactive and reactive way of helping with that.",
             author: 'Thomas Cloutier',
-            position: 'Vice President of Operations',
+            position: 'Vice President of Sales & Marketing',
           },
           {
             logo: 'fieldstone',
