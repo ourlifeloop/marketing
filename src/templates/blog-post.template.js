58635b8439b4af6423448e754088a3a033fe85ba
Mitchel Pigsley <mpigsley@gmail.com>
2021-04-28 08:45:57 -0600

    Make blog images constrained

--- src/templates/blog-post.template.js
+++ src/templates/blog-post.template.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GatsbyImage } from "gatsby-plugin-image";
+import { GatsbyImage } from 'gatsby-plugin-image';
 import { graphql } from 'gatsby';
 
 import BlogSubscriber from '../components/blog-subscriber';
@@ -16,34 +16,39 @@ export default function BlogPostTemplate({ data }) {
         {...frontmatter}
         postUrl={`${site.siteMetadata.displayUrl}${fields.slug}`}
         html={<div dangerouslySetInnerHTML={{ __html: html }} />}
-        photo={!!photo && <GatsbyImage image={photo.childImageSharp.gatsbyImageData} />}
+        photo={
+          !!photo && (
+            <GatsbyImage image={photo.childImageSharp.gatsbyImageData} />
+          )
+        }
       />
       <BlogSubscriber />
     </SiteWrapper>
   );
 }
 
-export const query = graphql`query BlogPostByPath($postId: String!) {
-  markdownRemark(id: {eq: $postId}) {
-    html
-    fields {
-      slug
-    }
-    frontmatter {
-      date(formatString: "MMMM DD, YYYY")
-      title
-      author
-      photo {
-        childImageSharp {
-          gatsbyImageData(quality: 80, layout: FULL_WIDTH)
+export const query = graphql`
+  query BlogPostByPath($postId: String!) {
+    markdownRemark(id: { eq: $postId }) {
+      html
+      fields {
+        slug
+      }
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        author
+        photo {
+          childImageSharp {
+            gatsbyImageData(quality: 80, layout: CONSTRAINED)
+          }
         }
       }
     }
-  }
-  site {
-    siteMetadata {
-      displayUrl
+    site {
+      siteMetadata {
+        displayUrl
+      }
     }
   }
-}
 `;
