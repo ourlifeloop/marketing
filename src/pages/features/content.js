a55f4646902ece9f81391c5cd967e2bb4ea9fd41
Levi Liester <levi.liester@gmail.com>
2022-09-28 16:33:25 -0500

    Add new Content page
    This still needs the images updated.  The copy should all be correct

---
+++ src/pages/features/content.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import { getImage } from 'gatsby-plugin-image';
+
+import FeatureWrapper from '../../components/feature-wrapper';
+import TitleSection from '../../primitives/title.section';
+import ImageSection from '../../primitives/image-section';
+
+export default function ContentFeature({ data, location }) {
+  return (
+    <FeatureWrapper
+      pathname={location.pathname}
+      title="Schedule, track and analyze resident activities and events on our powerful assisted living activity community calendar | LifeLoop"
+    >
+      <TitleSection header="Next Generation LifeLoop, with Content Powered by iN2L">
+        <p>
+          Introducing iN2L content in LifeLoop! By combining the power of iN2L’s person-centered
+          content with LifeLoop’s web and mobile app-based solutions, caregivers and residents can
+          now experience the impact of these two powerful solutions at their fingertips, no matter
+          where they are.
+        </p>
+      </TitleSection>
+      <ImageSection
+        leftImage
+        title="Engagement without Hardware, Connection without Limits"
+        description="Our combined goal is to empower your community to drive purposeful connection, meaningful engagement, and operational excellence…from any device! The connection and engagement your residents and families desire is now in one more powerful platform."
+        image={getImage(data.dataInformed)}
+      />
+      <ImageSection
+        rightImage
+        title="Get Content From Any Device in Any Location"
+        description="Take your senior living community to the next level by allowing residents to access content in their room or during formal activities. They can also find content customized to their unique, individual interests. On the staff side, they can easily utilize content in group or individual care settings, no matter what type of device they have."
+        image={getImage(data.timeSlots)}
+      />
+      <ImageSection
+        leftImage
+        title="Engage Residents at All Levels of Care"
+        description="In addition to being engaging and fun, the content library is purposefully designed and curated to address all the core dimensions of wellness, ensuring that communities can leverage a single solution for all levels of care, from independent living to memory care."
+        image={getImage(data.dataInformed)}
+      />
+      <ImageSection
+        rightImage
+        title="Building a Robust Activity Calendar Has Never Been Easier"
+        description="Cut down on the time it takes to plan purposeful activities by leveragingthe vast content library to engage residents in group activities. Easily utilize activity reporting to ensure you are offering enough activities and engagement to satisfy your residents’ changing needs."
+        image={getImage(data.integration)}
+      />
+    </FeatureWrapper>
+  );
+}
+
+export const query = graphql`
+  {
+    dataInformed: file(relativePath: { eq: "data-informed.png" }) {
+      childImageSharp {
+        gatsbyImageData(
+          height: 600
+          layout: CONSTRAINED
+          quality: 80
+          placeholder: BLURRED
+        )
+      }
+    }
+    integration: file(relativePath: { eq: "endless-integration.png" }) {
+      childImageSharp {
+        gatsbyImageData(
+          height: 600
+          layout: CONSTRAINED
+          quality: 80
+          placeholder: BLURRED
+        )
+      }
+    }
+    saveTime: file(relativePath: { eq: "save-time.png" }) {
+      childImageSharp {
+        gatsbyImageData(
+          height: 600
+          layout: CONSTRAINED
+          quality: 80
+          placeholder: BLURRED
+        )
+      }
+    }
+    timeSlots: file(relativePath: { eq: "time-slots.png" }) {
+      childImageSharp {
+        gatsbyImageData(
+          height: 600
+          layout: CONSTRAINED
+          quality: 80
+          placeholder: BLURRED
+        )
+      }
+    }
+  }
+`;
