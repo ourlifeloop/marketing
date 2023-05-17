d01222a8448f283f3f0e35be6530ea43cd335a95
Rafael Leon <rleon@in2l.com>
2023-05-17 15:48:08 -0400

    LIFELOOP-298 Submit form is still used by contact and recommend pages

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,3 +4,14 @@ export const subscribe = (email) =>
   fetch(`/.netlify/functions/subscribe?email=${email}`, {
     method: 'POST',
   }).then((response) => response.json());
+
+export const submitForm = (form, isSupport = false) =>
+  fetch('/.netlify/functions/form-submission', {
+    method: 'POST',
+    body: JSON.stringify({
+      isSupport,
+      form,
+      // eslint-disable-next-line
+      ['cc-num']: 'i-exist-so-hubspot-ignores-this-form',
+    }),
+  });
