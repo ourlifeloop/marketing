a4e1788e4c86cf9c7dc32ba5df6e1a320ba1c9b0
Levi Liester <levi.liester@gmail.com>
2024-04-10 13:26:47 -0500

    Use `connection` instead of `family`
    If you look at the redirects in static folder, you'll see we redirect from `family` to `connection`.  This causes the user type to not match and use the wrong welcome

--- src/components/user-training.js
+++ src/components/user-training.js
@@ -16,8 +16,8 @@ import { topic, topicHeader } from './user-training.module.scss';
 const headerByUserType = userType => {
   if (userType === 'staff') {
     return 'Welcome to the LifeLoop Learning Community, built especially for staff members! LifeLoop makes it easy for staff to manage a community. However, we understand that with the many features, it is a learning process to properly utilize LifeLoop. This page is dedicated to easing your learning experience and getting comfortable with using LifeLoop.';
-  } else if (userType === 'family') {
-    return 'Welcome to the LifeLoop Learning Community, built especially for family members! LifeLoop makes it easy for you to stay connected with your loved one and learn more about their daily activities. This page is dedicated to helping you learn the LifeLoop platform and answer any questions you may have.';
+  } else if (userType === 'connection') {
+    return 'Welcome to the LifeLoop Learning Community, built especially for connections! LifeLoop makes it easy for you to stay connected with your loved one and learn more about their daily activities. This page is dedicated to helping you learn the LifeLoop platform and answer any questions you may have.';
   }
   return 'Welcome to the LifeLoop Learning Community, built especially for residents! This page is dedicated to helping you get comfortable with using LifeLoop. Below you will be able to browse by topic, view FAQ’s and more. If you have any questions, please feel free to contact us at the phone number above or ask a community staff member for assistance.';
 };
